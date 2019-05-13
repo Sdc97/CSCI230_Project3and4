3a939a72949bd14a6d42bdc2e1d749ed66a2fd83
Sdc97 <Sdc977@gmail.com>
2019-05-12 21:21:52 -0700

    Problem 3 Completed

--- CSCI230_Project3-4/Question3-ELFHashing/HashMain.cpp
+++ CSCI230_Project3-4/Question3-ELFHashing/HashMain.cpp
@@ -1,20 +1,56 @@
-#define myMacro(a) (a * a)
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int myFunction(int a, int b=0);
+int ELFhash(char* key);
+
+int sfold(char* key);
 
 int main() {
-	int a[5] = { 1,2,3,4,5 };
-	int* b = a;
-	cout << myFunction(1,2) << endl;
-	cout << myFunction(1) << endl;
-	cout << myMacro(2.15) << endl;
+	cout << "This program compares the indexes of multiple sfold and ELFhash when placing the same string." << endl;
+
+	char hello[] = "HELLO WORLD";
+	cout << "\nThe string " << hello << " would hash to position " << sfold(hello) << " using sfold." << endl;
+	cout << "The string " << hello << " would hash to position " << ELFhash(hello) << " using ELFhash." << endl;
+	char hear[] = "NOW HEAR THIS";
+	cout << "\nThe string " << hear << " would hash to position " << sfold(hear) << " using sfold." << endl;
+	cout << "The string " << hear << " would hash to position " << ELFhash(hear) << " using ELFhash." << endl;
+	char now[] = "HEAR THIS NOW";
+	cout << "\nThe string " << now << " would hash to position " << sfold(now) << " using sfold." << endl;
+	cout << "The string " << now << " would hash to position " << ELFhash(now) << " using ELFhash." << endl;
+
 }
 
-int myFunction(int a, int b)
+int ELFhash(char* key)
 {
-	return a+b;
+	unsigned long h = 0;
+	while (*key) {
+		h = (h << 4) + *key++;
+		unsigned long g = h & 0xF0000000L;
+		if (g) {
+			h ^= g >> 24;
+		}
+		h &= ~g;
+	}
+	return h % 101;
+}
+
+int sfold(char* key)
+{
+	unsigned int* lkey = (unsigned int*)key;
+	int intlength = strlen(key) / 4;
+	unsigned int sum = 0;
+	for (int i = 0; i < intlength; i++)
+		sum += lkey[i];
+	
+	// Now deal with the extra chars at the end
+	int extra = strlen(key) - intlength*4;
+	char temp[4];
+	lkey = (unsigned int*)temp;
+	lkey[0] = 0;
+	for (int i = 0; i < extra; i++)
+		temp[i] = key[intlength * 4 + i];
+	sum += lkey[0];
+
+	return sum % 101;
 }
